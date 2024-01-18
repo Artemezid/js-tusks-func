5334b1c0174a52531c8cc9a635ec13e8970385d4
Artem <146821057+Artemezid@users.noreply.github.com>
2024-01-18 15:34:25 +0300

    Add files via upload

--- task-2 func.js
+++ task-2 func.js
@@ -1,15 +1,20 @@
-function simpleNumber(n) {
-  for(let i = 2; i < n; i++){
-    if(n % i === 0,n === 1,n ===0){
-      console.log("число - непростое");
-      return;
-    } else if (n > 1000) {
+function simpleNumber(n) {  
+  if (n <= 1) {
       console.log("данные неверны");
       return;
     }
-   }
+  
+    if (n > 1000) {
+      console.log("данные неверны");
+      return;
+    }
+  
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        console.log("число - составное");
+        return;
+      }
+    }
     console.log("число - простое");
-    return;
   }
-  simpleNumber(1);
-  /* всё равно на 0 и 1 пришет Ж простое число*/
+  simpleNumber(5);
